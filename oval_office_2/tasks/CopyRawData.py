7e0ff53553188790b4e3acd8647a428e3d790d6a
Dirk-Philip <dirkphilip@gmail.com>
2016-03-22 18:35:55 +0100

    The things we just changed

---
+++ oval_office_2/tasks/CopyRawData.py
@@ -0,0 +1,40 @@
+from oval_office_2 import utilities
+from . import task
+import click
+
+
+class CopyRawData(task.Task):
+    """Copies compiled binaries to relevant scratch directories.
+
+    For each SPECFEM3D_GLOBE run, compiled solver binaries, along with
+    information regarding topography, etc., are required in the run directory.
+    This function copies all relevant files from the specfem source directory.
+    """
+
+    def __init__(self, remote_machine, config):
+        super(CopyRawData, self).__init__(remote_machine, config)
+        self.event_info, self.iteration_info = utilities.get_lasif_information(
+            self.remote_machine, self.config.lasif_project_path,
+            self.config.base_iteration)
+
+    def check_pre_staging(self):
+        pass
+
+    def stage_data(self):
+        pass
+
+    def check_post_staging(self):
+        pass
+
+    def run(self):
+        all_events = sorted(self.event_info.keys())
+        with click.progressbar(all_events, label="Copying binaries...") as events:
+            for event in events:
+
+                raw_dir = os.path.join(self.config.lasif_project_path, "EVENTS",
+                                       event, "raw")
+                self.remote_machine.makedir(raw_dir)
+
+
+    def check_post_run(self):
+        pass
